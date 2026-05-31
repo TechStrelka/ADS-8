b74c7c87a2c90e19e3c1468d9182ca633a3e4422
Egor Soluynov <167007583+TechStrelka@users.noreply.github.com>
2026-05-31 17:55:30 +0300

    Update alg.cpp

--- src/alg.cpp
+++ src/alg.cpp
@@ -1,10 +1,53 @@
 // Copyright 2021 NNTU-CS
-#include  <iostream>
-#include  <fstream>
-#include  <locale>
-#include  <cstdlib>
-#include  "bst.h"
+#include "../include/bst.h"
+#include <iostream>
+#include <fstream>
+#include <vector>
+#include <algorithm>
+#include <string>
+#include <cctype>
 
 void makeTree(BST<std::string>& tree, const char* filename) {
-  // поместите сюда свой код
+    std::ifstream file(filename);
+
+    std::string current_word = "";
+    char ch;
+
+    while (file.get(ch)) {
+        if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z')) {
+            current_word += std::tolower(static_cast<unsigned char>(ch));
+        }
+        else {
+            if (!current_word.empty()) {
+                tree.add(current_word);
+                current_word = "";
+            }
+        }
+    }
+    if (!current_word.empty()) {
+        tree.add(current_word);
+    }
+
+    file.close();
+}
+
+void printFreq(BST<std::string>& tree) {
+    std::vector<std::pair<std::string, int>> words;
+    tree.getAllElements(words);
+
+    std::sort(words.begin(), words.end(), [](const std::pair<std::string, int>& a, const std::pair<std::string, int>& b) {
+        if (a.second != b.second) {
+            return a.second > b.second;
+        }
+        return a.first < b.first;
+        });
+
+    std::ofstream outFile("../result/freq.txt");
+
+    for (const auto& pair : words) {
+        std::cout << pair.first << ": " << pair.second << std::endl;
+        outFile << pair.first << " " << pair.second << "\n";
+    }
+
+    outFile.close();
 }
