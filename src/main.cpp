f6b6ef56ea059ced06f27a12577df6d802a7d0de
Egor Soluynov <167007583+TechStrelka@users.noreply.github.com>
2026-05-31 17:55:59 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,26 @@
 // Copyright 2021 NNTU-CS
-#include "bst.h"
+#include "../include/bst.h"
+#include <iostream>
+#include <string>
+
+void makeTree(BST<std::string>& tree, const char* filename);
+void printFreq(BST<std::string>& tree);
 
 int main() {
+    BST<std::string> tree;
+
+    makeTree(tree, "../src/war_peace.txt");
+
+    std::cout << "Глубина дерева: " << tree.depth() << std::endl;
+
+    if (tree.search("war")) {
+        std::cout << "Слово 'war' найдено" << std::endl;
+    }
+    else {
+        std::cout << "Слово 'war' не найдено" << std::endl;
+    }
+
+    printFreq(tree);
+
     return 0;
 }
